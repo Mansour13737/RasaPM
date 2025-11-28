27fd9d78618a2d26ff2f3fe15a01a280f5c0c8a1
Mansour <mansournosrati6973@gmail.com>
2025-11-28 15:57:27 +0000

    کلا پروژه رو برگردون به قبل از اتصال به دیتا بیس (هم ui هم موارد دیگه)

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,19 +1,17 @@
 
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
 // TODO: Replace with your actual Firebase project configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBcTYDGhd1u3thpI7pOuDilV-cj7ZoyAu8",
   authDomain: "studio-7414263376-fc0fd.firebaseapp.com",
   projectId: "studio-7414263376-fc0fd",
-  storageBucket: "studio-7414263376-fc0fd.firebasestorage.app",
+  storageBucket: "studio-7414263376-fc0fd.appspot.com",
   messagingSenderId: "527954872834",
   appId: "1:527954872834:web:b9bdaad9f001397c93b51b"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 
-export { app, auth };
+export { app };
